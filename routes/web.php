9e48410fe0d0d598a3978054914cf2dadd5ef73f
Jayy-develop <jayapenting92@gmail.com>
2025-11-04 22:47:44 +0700

    integrasi landing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/welcome',[HomeController::class, 'index'])->name('welcome');
