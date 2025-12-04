c9e4262f38c8c1d77d97057c2de88e688cdc5d97
deru <derupratama03@gmail.com>
2025-12-04 12:24:45 +0700

    cek cek

--- admin/penjualan_history.php
+++ admin/penjualan_history.php
@@ -1,7 +1,7 @@
   <?php
 
   $dataPenjualan = query("SELECT * FROM penjualan
-  WHERE statusPenjualan IN ('Gagal', 'Selesai', 'Diproses')
+  WHERE statusPenjualan IN ('Selesai')
   ORDER BY idPenjualan DESC");
 
 
